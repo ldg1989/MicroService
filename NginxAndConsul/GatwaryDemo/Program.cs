3df1fc1850ee1291fa73d7b897a89e2465b350af
641489189@qq.com <641489189@qq.com>
2020-07-12 23:47:51 +0800

    11

--- NginxAndConsul/GatwaryDemo/Program.cs
+++ NginxAndConsul/GatwaryDemo/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -16,8 +17,15 @@ namespace GatwaryDemo
       CreateHostBuilder(args).Build().Run();
     }
 
+    //dotnet XXX.dll --urls="http://*:6666" --ip="127.0.0.1" --port=6666
+
+
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+      .ConfigureAppConfiguration(conf =>
+      {
+        conf.AddJsonFile("configuration.json", optional: false, reloadOnChange: true);
+      })
             .ConfigureWebHostDefaults(webBuilder =>
             {
               webBuilder.UseStartup<Startup>();
